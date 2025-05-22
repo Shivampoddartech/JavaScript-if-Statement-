6e3153a65898e32dd1b17ac6c272db958688a674
shivam <shivampoddartech@gmail.com>
2025-05-22 23:49:53 +0530

    if ifelse else

--- app.js
+++ app.js
@@ -21,17 +21,96 @@
 // console.log(` welcome : ${firstName} poddar`)
 // }
 
-console.log(" trafic Light system");
-let color = "Yellow";
-if (color == "red"){
-    console.log("color is red  its Danger  now you stop ")
 
-}
-if (color == "Yellow"){
-    console.log("color is Yellow  its   time to wait   ")
 
-}
-if (color == "Green"){
-    console.log("color is Green means you go  now  ")
+// console.log(" trafic Light system");
+// let color = "Yellow";
+// if (color == "red"){
+//     console.log("color is red  its Danger  now you stop ")
 
+// }
+// if (color == "Yellow"){
+//     console.log("color is Yellow  its   time to wait   ")
+
+// }
+// if (color == "Green"){
+//     console.log("color is Green means you go  now  ")
+
+// }
+
+// console.log("Using if else");
+
+// let age = 10;
+// if ( age >= 18){
+// console.log("you can vote");
+// } 
+// else if( age < 18)
+// console.log("you canot vote");
+
+
+
+// let age = 14;
+// if ( age>=18 ){
+// console.log("you can vote")
+// }
+// else if( age< 18){
+// console.log("you are not able to vote")
+// }
+
+
+// console.log("school grade");
+// let marks = 62
+
+// if (marks >=85){
+//     console.log(" you got A++")
+// }
+
+// else if (marks >=60 ){
+// console.log("you got A+");
+// }
+
+// else if (marks >=59 ){
+// console.log("you got B");
+// }
+
+// else if (marks >=33){
+// console.log("you got C");
+// }
+
+// else if (marks <33 ){
+// console.log("you got F");
+// }
+
+
+
+// console .log("Else Statement");
+// let age = 18;
+// if ( age >=18){
+// console.log("you can vote"); 
+// }
+// else {
+//     console.log("you cant vote");
+// }
+
+
+console.log("Calculate the price");
+let price = 100;
+if ( price >= 250){
+console.log(" size is xl price is 250");
 }
+
+else if( price >= 200){
+console.log(" size is L price is 200 ");
+}
+
+else if( price >= 100){
+console.log(" size is M price is 100");
+}
+
+else if( price >= 50){
+console.log(" size is s price is 50");
+}
+else {
+    console.log("invalid");
+}
+
