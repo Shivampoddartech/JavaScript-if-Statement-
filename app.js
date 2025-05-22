5a0d318806c27da0f9db7c2cea6955cc8d24d4ca
shivam <shivampoddartech@gmail.com>
2025-05-22 16:57:07 +0530

    Traffic light system

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-console .log(" write js code using if");
-let age = 14;
-if ( age >= 18){
-console.log("you can vote")
-console.log("you can right to marriaage")
+// console .log("checking age Elegiblity");
+// let age = 21;
+// if ( age >= 18){
+//     console.log("you can vote")
+//    console.log("you have right to live life indpedent")
+// }
+// console.log("invalid / you are aunder age");
+
+
+
+// let firstName = "shivam";
+// if (firstName == "shivam"){
+// console.log(` welcome  ${firstName} poddar `);
+// }
+
+
+
+// console.log (" shivam poddar");
+// let firstName  = "shivam";
+// if ( firstName =="shivam"){
+// console.log(` welcome : ${firstName} poddar`)
+// }
+
+console.log(" trafic Light system");
+let color = "Yellow";
+if (color == "red"){
+    console.log("color is red  its Danger  now you stop ")
+
 }
-console.log("you are under age");
+if (color == "Yellow"){
+    console.log("color is Yellow  its   time to wait   ")
+
+}
+if (color == "Green"){
+    console.log("color is Green means you go  now  ")
+
+}
