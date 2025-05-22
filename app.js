811c7ef18ba99adb0c8736a536603ec47a367375
shivam <shivampoddartech@gmail.com>
2025-05-22 16:06:24 +0530

    using js create if Statement

---
+++ app.js
@@ -0,0 +1,7 @@
+console .log(" write js code using if");
+let age = 14;
+if ( age >= 18){
+console.log("you can vote")
+console.log("you can right to marriaage")
+}
+console.log("you are under age");
